ec30102c63fbf6815e4e2026df4b01294cf8a588
xudonglei <xudonglei2020@163.com>
2020-04-18 17:23:37 +0800

    添加好ZLG的板子

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_14x14_evk_nand_defconfig
+
+make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j8
